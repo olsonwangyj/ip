48e931eb5ec5ff746f3ab5380abbcdfe2b69dd22
olsonwangyj <olsonwangyj@gmail.com>
2025-01-20 16:31:52 +0800

    Renamed Duke to Litchi and implemented an initial skeletal version

---
+++ src/main/java/Litchi.java
@@ -0,0 +1,11 @@
+public class Litchi {
+    public static void main(String[] args) {
+        String indentations = "_____________________________________________";
+        System.out.println(indentations);
+        System.out.println("Hello! I'm Litchi");
+        System.out.println("What can I do for you?");
+        System.out.println(indentations);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(indentations);
+    }
+}
