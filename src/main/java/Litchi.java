69a375f90cc67d8ad3e6263a5f6b38eec70384bd
olsonwangyj <olsonwangyj@gmail.com>
2025-01-31 12:02:06 +0800

    Implement Echo feature

--- src/main/java/Litchi.java
+++ src/main/java/Litchi.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Litchi {
     public static void main(String[] args) {
         String indentations = "_____________________________________________";
@@ -5,7 +7,21 @@ public class Litchi {
         System.out.println("Hello! I'm Litchi");
         System.out.println("What can I do for you?");
         System.out.println(indentations);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(indentations);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String in = scanner.nextLine().trim();
+
+            if (in.equals("bye")) {
+                System.out.println(indentations);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(indentations);
+                break;
+            }
+
+            System.out.println(indentations);
+            System.out.println(in);
+            System.out.println(indentations);
+        }
     }
 }
